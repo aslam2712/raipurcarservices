2348919ef077ecc6142e220bd7d8dc3151d60c31
Mohammad Aslam <shahnazansari@Aslam.local>
2024-12-10 21:26:46 +0530

    project creation

---
+++ src/main/java/raipurcarservices/carservices/CarservicesApplication.java
@@ -0,0 +1,13 @@
+package raipurcarservices.carservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/raipurcarservices/carservices/CarservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package raipurcarservices.carservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
